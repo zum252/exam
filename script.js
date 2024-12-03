b210fe23dd43ce90f5c47a094e2cf137fd796396
zum252 <zumradkhamzaeva9@gmail.com>
2024-12-03 20:30:38 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+let ism = prompt("Ismingizni kiriting:");
+let yosh = prompt("Yoshingizni kiriting:");
+
+let userInfoDiv = document.createElement("div");
+userInfoDiv.id = "userInfo";
+let user = prompt;
+let dev = prompt;
+userInfoDiv.innerHTML = 'Ismingiz: Zumrad<br>Yoshingiz: 12';
+
+
+document.body.appendChild(userInfoDiv);
